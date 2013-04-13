b37f5001fabc51733908e481e30fb578e60f6e8d
Jack Callister <jarsbe@gmail.com>
2013-04-13 12:18:14 +1200

    Added compiler to manifest.

--- lib/generators/smacssify/install_generator.rb
+++ lib/generators/smacssify/install_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 module Smacssify
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../../", __FILE__)
-    
+
     desc "This generator sets up the smacss directory structure"
 
     # Commandline options can be defined here using Thor-like options:
@@ -12,12 +12,24 @@ module Smacssify
     # I can later access that option using:
     # options[:my_opt]
 
-    # def self.source_root
-    #   @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-    # end
-
     def add_assets
       directory "assets/stylesheets", "app/assets/stylesheets"
+
+      css_manifest = "app/assets/stylesheets/application.css"
+
+      # Thanks https://github.com/seyhunak/twitter-bootstrap-rails/ for bits of this code!
+      if File.exist?(css_manifest)
+        # Add our own require:
+        content = File.read(css_manifest)
+        if content.match(/require_tree\s+\.\s*$/)
+          # We don't want to include the tree - just our compiler.css.scss
+          gsub_file(css_manifest, /require_tree\s+\.\s*$/, "")
+        end
+        style_require_block = " *= require ./compiler\n"
+        insert_into_file css_manifest, style_require_block, :after => "require_self\n"
+      else
+        copy_file "/assets/templates/application.css.scss", "app/assets/stylesheets/application.css.scss"
+      end
     end
 
     # Generator Code. Remember this is just suped-up Thor so methods are executed in order
